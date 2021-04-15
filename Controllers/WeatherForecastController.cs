2034949bb9f463f426f35b7bbe353dd51a07c730
stupidamigo <amit.sharma.tafs@gmail.com>
2021-04-15 18:01:53 +0000

    date format long

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,7 +29,7 @@ namespace WeatherMicroSvc.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index).ToShortDateString(),
+                Date = DateTime.Now.AddDays(index).ToLongDateString(),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
